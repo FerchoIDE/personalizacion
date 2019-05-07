2551e0f1e5e6dc4050a10f7e4783879418dfeee8
FerchoIDE <fmartinez@consistent.com.mx>
2019-05-07 11:41:32 -0500

    Integracion a respaldo

--- modules/GeneratorViewClient/src/main/java/generatorviewclient/api/IJournalApi.java
+++ modules/GeneratorViewClient/src/main/java/generatorviewclient/api/IJournalApi.java
@@ -8,10 +8,11 @@ import com.liferay.portal.kernel.json.JSONArray;
 
 public interface IJournalApi {
 
+ 
     /**
-     * M�todo getListJournalFoldersByCode
-     * @param groupId Id del s�tio
-     * @param codeBrand codigo carpeta donde se hace la b�squeda
+     * Mï¿½todo getListJournalFoldersByCode
+     * @param groupId Id del sï¿½tio
+     * @param codeBrand codigo carpeta donde se hace la bï¿½squeda
      * @return <ul>
      *  <li>case 1: code_hotel!=null && brand!=null JSONArray </li>
      *  <li>case 2: brand!=null && code_hotel==null JSONArray </li>
@@ -22,9 +23,9 @@ public interface IJournalApi {
      */
     public JSONArray getListJournalFoldersByCode(Long groupId,Long codeBrand) throws PortalException;
     /**
-     * M�todo getWCByFolder
-     * @param groupId Id del s�tio
-     * @param folderID codigo carpeta/marca donde se hace la b�squeda
+     * Mï¿½todo getWCByFolder
+     * @param groupId Id del sï¿½tio
+     * @param folderID codigo carpeta/marca donde se hace la bï¿½squeda
      * @return <ul>
      *  <li>case 1: code_hotel!=null && brand!=null JSONArray </li>
      *  <li>case 2: brand!=null && code_hotel==null JSONArray </li>
@@ -35,11 +36,11 @@ public interface IJournalApi {
      */
     public List<JournalArticle> getWCByFolder(Long groupId,Long folderId) throws PortalException;
     /**
-     * M�todo createFolderNestedFolderId
-     * @param groupId Id del s�tio
-     * @param brand Nombre de la carpeta marca donde se hace la b�squeda
+     * Mï¿½todo createFolderNestedFolderId
+     * @param groupId Id del sï¿½tio
+     * @param brand Nombre de la carpeta marca donde se hace la bï¿½squeda
      * @param type tipo de archivo a buscar(No se filtra)
-     * @param code_hotel Nombre de la carpeta c�difgo de hotel para la b�squeda
+     * @param code_hotel Nombre de la carpeta cï¿½difgo de hotel para la bï¿½squeda
      * @return <ul>
      *  <li>case 1: code_hotel!=null && brand!=null JSONArray </li>
      *  <li>case 2: brand!=null && code_hotel==null JSONArray </li>
@@ -50,11 +51,11 @@ public interface IJournalApi {
      */
     public JSONArray createFolderNestedFolderId(Long userId,Long groupId,Long parentFolderId,String name) throws PortalException;
     /**
-     * M�todo getWCByName
-     * @param groupId Id del s�tio
-     * @param brand Nombre de la carpeta marca donde se hace la b�squeda
+     * Mï¿½todo getWCByName
+     * @param groupId Id del sï¿½tio
+     * @param brand Nombre de la carpeta marca donde se hace la bï¿½squeda
      * @param type tipo de archivo a buscar(No se filtra)
-     * @param code_hotel Nombre de la carpeta c�difgo de hotel para la b�squeda
+     * @param code_hotel Nombre de la carpeta cï¿½difgo de hotel para la bï¿½squeda
      * @return <ul>
      *  <li>case 1: code_hotel!=null && brand!=null JSONArray </li>
      *  <li>case 2: brand!=null && code_hotel==null JSONArray </li>
@@ -65,11 +66,11 @@ public interface IJournalApi {
     public List<JournalArticle> getWCByName(Long groupId,String brand,String code_hotel,String name) throws PortalException;
     //busqueda recursiva de webcontents x M y CH getAllWCAndJournalFolder((portletGroupId,"AQUA","","AQC");
     /**
-     * M�todo getWCAndJournalFolder
-     * @param groupId Id del s�tio
-     * @param brand Nombre de la carpeta marca donde se hace la b�squeda
+     * Mï¿½todo getWCAndJournalFolder
+     * @param groupId Id del sï¿½tio
+     * @param brand Nombre de la carpeta marca donde se hace la bï¿½squeda
      * @param type tipo de archivo a buscar(No se filtra)
-     * @param code_hotel Nombre de la carpeta c�difgo de hotel para la b�squeda
+     * @param code_hotel Nombre de la carpeta cï¿½difgo de hotel para la bï¿½squeda
      * @return <ul>
      *  <li>case 1: code_hotel!=null && brand!=null JSONArray </li>
      *  <li>case 2: brand!=null && code_hotel==null JSONArray </li>
@@ -81,11 +82,11 @@ public interface IJournalApi {
     public List<JournalArticle> getWCAndJournalFolder(Long groupId,String brand,String code_hotel) throws PortalException;
     //busqueda recursiva de webcontents x M, CH y tipo getWCAndJournalFolderType((portletGroupId,"AQUA","","AQC");
     /**
-     * M�todo getWCAndJournalFolderType
-     * @param groupId Id del s�tio
-     * @param brand Nombre de la carpeta marca donde se hace la b�squeda
+     * Mï¿½todo getWCAndJournalFolderType
+     * @param groupId Id del sï¿½tio
+     * @param brand Nombre de la carpeta marca donde se hace la bï¿½squeda
      * @param type tipo de archivo a buscar(No se filtra)
-     * @param code_hotel Nombre de la carpeta c�difgo de hotel para la b�squeda
+     * @param code_hotel Nombre de la carpeta cï¿½difgo de hotel para la bï¿½squeda
      * @return <ul>
      *  <li>case 1: code_hotel!=null && brand!=null JSONArray </li>
      *  <li>case 2: brand!=null && code_hotel==null JSONArray </li>
@@ -95,7 +96,8 @@ public interface IJournalApi {
      *  @throws PortalException
      */
     public List<JournalArticle> getWCByJournalFolderAndStructureType(Long groupId,String brand,String code_hotel,String type) throws PortalException;
-
+   
+  
 
     public JournalArticle saveWC(String json) throws PortalException;
 
@@ -107,51 +109,87 @@ public interface IJournalApi {
 
     /*busqueda tosdos los contenidos recursivos de un tipo
     groupid,
-    marca,(podria ser opcional)
-    code hotel(podria ser opcional)
+    marca,(podria ser opcional) Convierte la marca en el Id del folder
+    code hotel(podria ser opcional)Convierte la Hotel en el Id del folder
     tipo de contenido (Id de la estructura) si este es null es sobre todos los contenidos
      */
-    /*
-    ejemplo de invocacion
-    21331,FI,GAL,habitacion
-     */
     public List<JournalArticle> getWebcontentRecursiveByType(Long groupId,String marca,String codeHotel,String TypeContent) throws PortalException;
-
+    
+    /*busqueda tosdos los contenidos recursivos de un tipo y folder en especifico
+     * 
+     Long groupId, Id del sitio 123 Long
+     Long folderId, idFolder donde comenzara la busqueda 12344 Long
+     Long typeContent id de la estructura 12345 Long
+    groupid,
+      */
     public List<JournalArticle> getWebcontentRecursiveByType(Long groupId,Long folderId,Long typeContent) throws PortalException;
 
+    
+    /*busqueda tosdos los contenidos recursivos de un tipo y folder en especifico
+     * 
+     Long groupId, Id del sitio 12345 Long 
+     String marca, Toma la maraca y extrae el valor del id FA String
+     String codeHotel, Toma el valor del folder Hotel  FAA String
+     Long typeContent id de la estructura 12345 Long
+      */
     public List<JournalArticle> getWebcontentRecursiveByType(Long groupId,String marca,String codeHotel,Long typeContent) throws PortalException;
 
 
 
     /*busqueda todos los contenidos recursivos de un tipo que conincidan en el titulo
-    groupid,
-    marca,(podria ser opcional)
-    code hotel(podria ser opcional)
-    tipo de contenido (Id de la estructura) si este es null es sobre todos los contenidos
-    titulo del contenido, considerar usar like y no sensible a mayusculas minusculas
+    groupid, 12345 Long
+    marca,(podria ser opcional) FA
+    code hotel(podria ser opcional) FAA 
+    tipo de contenido (Id de la estructura) si este es null es sobre todos los contenidos Hotels
+    titulo del contenido, considerar usar like y no sensible a mayusculas minusculas Sushi Corner - FCC
      */
     public List<JournalArticle> getWebcontentRecursiveByTitle(Long groupId,String marca,String codeHotel,String title,String contentType) throws PortalException;
-
+    /*Obtiene los Web contents dentro del nombre de folder marca -> Hotel  que se le indique ademas del FolderId
+     * Long groupId Nombre del sitio 1234 Long 
+     * folderId Id de folder donde comenzara la busqueda
+     * String name Nombre del titulo a buscar  
+     * 
+     * */
     public List<JournalArticle> getWebcontentRecursiveByTitle(Long groupId,Long folderId,String title,Long structureId) throws PortalException;
 
+    /*Obtiene los Web contents dentro del nombre de folder marca -> Hotel  que se le indique ademas del FolderId
+     Long groupId,12345
+     String marca, FA
+     String codeHotel, FAA
+     String title, Fiesta ... 
+     Long structureId 12345
+     * 
+     * */
     public List<JournalArticle> getWebcontentRecursiveByTitle(Long groupId,String marca,String codeHotel,String title,Long structureId) throws PortalException;
 
-
-    /*busqueda todos los contenidos recursivos dentro de un folder
-    groupid,
-    folderid
-    tipo de contenido (Id de la estructura) si este es null es sobre todos los contenidos
-     */
+    /*Obtiene los Web contents dentro del nombre de folder marca -> Hotel  que se le indique ademas del FolderId
+     * Long groupId Nombre del sitio
+     * String brand Nombre de la carpeta marca
+     * String code_hotel Nombre del carpeta de hotel 
+     * String name Nombre del titulo a buscar  
+     *  Long structureId Id de la estructura 
+     * */
     public List<JournalArticle> getWCAndJournalFolderByName(Long groupId,String brand,String code_hotel,String name) throws PortalException;
-
+ 
+    /*Obtiene los Web contents dentro del nombre de folder marca -> Hotel  que se le indique ademas del FolderId
+     * Long groupId Nombre del sitio
+     * String brand Nombre de la carpeta marca
+     * String code_hotel Nombre del carpeta de hotel 
+     * String name Nombre del titulo a buscar 
+     * Long structureId Id de la estructura 
+     * 
+     * */
     public List<JournalArticle> getWCAndJournalFolderByName(Long groupId,String brand,String code_hotel,String name,Long structureId) throws PortalException;
-
+    /*Obtiene los Web contents dentro del folderId que se le indique*/
     public List<JournalArticle> getWCByFolderId(Long groupId, Long folderId) throws PortalException;
 
     /*todas las carpetas dentro de
-    groupid,
-    marca,(podria ser opcional)
-    code hotel(podria ser opcional)
+    groupid, id del sitio 
+    brand, Nombre de la carpeta marca
+    code hotel Nombre de la carpeta Hotel
+    
+     public JSONArray getListJournalFolders(Long groupId,String brand,String code_hotel) throws PortalException;
+
      */
 
     public JSONArray getListJournalFolders(Long groupId,String brand,String code_hotel) throws PortalException;
@@ -159,15 +197,34 @@ public interface IJournalApi {
 
     /*buscar si un codigo de hotel ya existe, si existe indicar el titulo dle hotel
     code hotel
+    Busca sin recursividad, solo obtiene el primer nivel 
+    groupId Id del sítio
+    structureName Nombre de la estructura Hotels
+    code Es el codigo de Hotel dentro del xml
+    
+        public List<JournalArticle> searchWebContentByCodeHotelFirstLevel(Long groupId,String structureName,String code) throws PortalException;
 
      */
     public List<JournalArticle> searchWebContentByCodeHotelFirstLevel(Long groupId,String structureName,String code) throws PortalException;
+  
+    /*buscar si un codigo de hotel ya existe, si existe indicar el titulo dle hotel
+    code hotel
+    Busca sin recursividad, solo obtiene el primer nivel 
+    groupId Id del sítio
+    structureName Nombre de la estructura Hotels
+    folderId Parte a través del folder que le indiques 
+    code Es el codigo de Hotel dentro del xml
+    
+        public List<JournalArticle> searchWebContentByCodeHotelFirstLevel(Long groupId,String structureName,String code) throws PortalException;
 
+     */
     public List<JournalArticle> searchWebContentByCodeHotelFirstLevelByFolder(Long groupId,String structureName,String code,Long folderId) throws PortalException;
 
 
     /* un arreglo de listado de marcas
     groupid,
+    
+    Obtiene todas la carpetas de origen es decir despues de la base Posadas/Hotel que es donde se encuentra el listado de folder que son los folder de marca
      */
 
     public JSONArray getListJournalFoldersBrand(Long groupId) throws PortalException;
@@ -175,16 +232,33 @@ public interface IJournalApi {
     /* un arreglo de listado hoteles
     groupid,
     marca
+    Nota: Todo parte de la ruta base
+    
+    marca/brand Id del folder de marca o cualquier folder
+    groupid Id del sitio
+    getListJournalFoldersByBrand(groupid,brand);
      */
-
+    
     public JSONArray getListJournalFoldersByBrand(Long groupId,Long brand) throws PortalException;
-
-
+    
+   
+    
+    /* un arreglo de listado hoteles
+    groupid,
+    marca
+    Nota: Todo parte de la ruta base
+   
+    brand/marca Busca a través de un string el nombre de la carpeta,convierte en Id y hace la misma operación 
+                 que el método de arriba.
+           
+    groupid Id del sitio
+    getListJournalFoldersByBrand(groupid,brand);
+     */
     public JSONArray getListJournalFoldersByBrand(Long groupId,String brand) throws PortalException;
 
-
+   
     /*
-    Recuperar un contenido y la estructura asociada a este
+    Recuperar un contenido y la estructura asociada a este IdArticle Map 
 
      */
 }
--- modules/GeneratorViewClient/src/main/java/generatorviewclient/portlet/resource/GetJournalsResource.java
+++ modules/GeneratorViewClient/src/main/java/generatorviewclient/portlet/resource/GetJournalsResource.java
@@ -34,89 +34,86 @@ import java.util.*;
         service = MVCResourceCommand.class
 )
 public class GetJournalsResource implements MVCResourceCommand {
-    @Override
-    public boolean serveResource(ResourceRequest resourceRequest, ResourceResponse resourceResponse) throws PortletException {
-        System.out.println("--------------+++++++++------");
-        try {
-            ThemeDisplay themeDisplay = (ThemeDisplay) resourceRequest.getAttribute(WebKeys.THEME_DISPLAY);
-            long portletGroupId = themeDisplay.getLayout().getGroupId();
-            String body = FileUtil.getBuffer(resourceRequest.getReader());
-            JSONObject jsonObject = new JSONObject(body);
-            System.out.println(jsonObject);
-            resourceResponse.setCharacterEncoding("UTF-8");
-            resourceResponse.setContentType("application/json");
-            String result = new JSONArray(getData()).toString();
-            System.out.println(result);
+     	 @Override
+    	    public boolean serveResource(ResourceRequest resourceRequest, ResourceResponse resourceResponse) throws PortletException {
+    	        System.out.println("--------------+++++++++------");
+    	        try {
+    	            ThemeDisplay themeDisplay = (ThemeDisplay) resourceRequest.getAttribute(WebKeys.THEME_DISPLAY);
+    	            long portletGroupId = themeDisplay.getLayout().getGroupId();
+    	            String body = FileUtil.getBuffer(resourceRequest.getReader());
+    	            JSONObject jsonObject = new JSONObject(body);
+    	            System.out.println(jsonObject);
+    	            resourceResponse.setCharacterEncoding("UTF-8");
+    	            resourceResponse.setContentType("application/json");
+
+    	            String brand = jsonObject.getString("brand");
+    	            String codeHotel = jsonObject.getString("codeHotel");
+    	            List<JournalArticle> array = new LinkedList<>();
+    	            if (jsonObject.has("folderId")) {
+
+    	                // array = new JournalApi().getFilesByCurrentFolderAndName(portletGroupId,jsonObject.getLong("folderId"),"");
+    	            } else if (jsonObject.has("nameFolder")) {
+    	                // array = new JournalApi().getWCByName(portletGroupId,brand,jsonObject.getString("nameFolder"),codeHotel);
+    	                //getWCByName
+    	            } else {
+    	                array = new JournalApi().getWebcontentRecursiveByType(portletGroupId, brand, codeHotel, "Habitación");
+    	                //getListJournalFolders
+    	            }
+    	            List lsResult = new LinkedList();
+
+    	            array.forEach(journalArticle -> {
+    	                Map<String, Object> mpObject = new HashMap<>();
+    	                mpObject.put("description", journalArticle.getDescriptionMap());
+    	                mpObject.put("id", journalArticle.getId());
+    	                mpObject.put("image", journalArticle.getArticleImageURL(themeDisplay));
+    	                mpObject.put("title", journalArticle.getTitleCurrentValue());
+    	                long _month = Period.between(journalArticle.getStatusDate().toInstant().atZone(ZoneId.systemDefault())
+    	                        .toLocalDate(), LocalDate.now()).toTotalMonths();
+    	                mpObject.put("date", String.format("%d meses",_month));
+    	                mpObject.put("status", WorkflowConstants.getStatusLabel(journalArticle.getStatus()));
+    	                mpObject.put("user", journalArticle.getStatusByUserName());
+    	                try {
+    	                    mpObject.put("structureName",journalArticle.getDDMStructure().getNameCurrentValue());
+    	                    mpObject.put("structureId",journalArticle.getDDMStructure().getStructureId());
+    	                }catch (Exception e){}
 
 
-            String brand = jsonObject.getString("brand");
-            String codeHotel = jsonObject.getString("codeHotel");
-            List<JournalArticle> array = new LinkedList<>();
-            if (jsonObject.has("folderId")) {
+    	                try {
+    	                    mpObject.put("path", fullPath(journalArticle.getFolder()));
+    	                }catch(Exception ex){
 
-                // array = new JournalApi().getFilesByCurrentFolderAndName(portletGroupId,jsonObject.getLong("folderId"),"");
-            } else if (jsonObject.has("nameFolder")) {
-                // array = new JournalApi().getWCByName(portletGroupId,brand,jsonObject.getString("nameFolder"),codeHotel);
-                //getWCByName
-            } else {
-                array = new JournalApi().getWebcontentRecursiveByType(portletGroupId, brand, codeHotel, "HabitaciÃ³n");
-                //getListJournalFolders
-            }
-            List lsResult = new LinkedList();
+    	                }
 
-            array.forEach(journalArticle -> {
-                Map<String, Object> mpObject = new HashMap<>();
-                mpObject.put("description", journalArticle.getDescriptionMap());
-                mpObject.put("id", journalArticle.getId());
-                mpObject.put("image", journalArticle.getArticleImageURL(themeDisplay));
-                mpObject.put("title", journalArticle.getTitleCurrentValue());
-                long _month = Period.between(journalArticle.getStatusDate().toInstant().atZone(ZoneId.systemDefault())
-                        .toLocalDate(), LocalDate.now()).toTotalMonths();
-                mpObject.put("date", String.format("%d meses",_month));
-                mpObject.put("status", WorkflowConstants.getStatusLabel(journalArticle.getStatus()));
-                mpObject.put("user", journalArticle.getStatusByUserName());
-                try {
-                    mpObject.put("structureName",journalArticle.getDDMStructure().getNameCurrentValue());
-                    mpObject.put("structureId",journalArticle.getDDMStructure().getStructureId());
-                }catch (Exception e){}
+    	                 lsResult.add(mpObject);
 
+    	            });
 
-                try {
-                    mpObject.put("path", fullPath(journalArticle.getFolder()));
-                }catch(Exception ex){
+    	            resourceResponse.getPortletOutputStream().write(new JSONArray(lsResult).toString().getBytes());
+    	        } catch (Exception e) {
+    	            e.printStackTrace();
+    	            try {
+    	                resourceResponse.getWriter().write(e.getMessage());
+    	            } catch (IOException e1) {
+    	                e1.printStackTrace();
+    	            }
+    	            return true;
+    	        }
+    	        return false;
+    	    }
+    	    protected String fullPath(JournalFolder folder)  {
+    	        String folderName = folder.getName();
+    	        JournalFolder parent = null;
+    	        try {
+    	            parent = folder.getParentFolder();
+    	        } catch (PortalException e) {
+    	            e.printStackTrace();
+    	        }
 
-                }
-
-                 lsResult.add(mpObject);
-
-            });
-
-            resourceResponse.getPortletOutputStream().write(new JSONArray(lsResult).toString().getBytes());
-        } catch (Exception e) {
-            e.printStackTrace();
-            try {
-                resourceResponse.getWriter().write(e.getMessage());
-            } catch (IOException e1) {
-                e1.printStackTrace();
-            }
-            return true;
-        }
-        return false;
-    }
-    protected String fullPath(JournalFolder folder)  {
-        String folderName = folder.getName();
-        JournalFolder parent = null;
-        try {
-            parent = folder.getParentFolder();
-        } catch (PortalException e) {
-            e.printStackTrace();
-        }
-
-        if (parent == null) {
-            return "/" + folderName;
-        }
-        else {
-            return fullPath(parent) +"/"+ folderName;
-        }
-    }
+    	        if (parent == null) {
+    	            return "/" + folderName;
+    	        }
+    	        else {
+    	            return fullPath(parent) +"/"+ folderName;
+    	        }
+    	    }
 }
